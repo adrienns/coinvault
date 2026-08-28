1de5468f58bd47da66a4c7d2f3987170b5f824bd
adrienns <adrienn.sepa@outlook.com>
2026-08-28 12:26:38 +0700

    fix: set batchMaxCount to 1 on Holesky provider

--- lib/web3/provider.ts
+++ lib/web3/provider.ts
@@ -4,5 +4,6 @@ import { HOLESKY_CHAIN_ID, HOLESKY_RPC_URL } from "@/lib/web3/constants"
 export function createHoleskyProvider() {
   return new ethers.JsonRpcProvider(HOLESKY_RPC_URL, HOLESKY_CHAIN_ID, {
     staticNetwork: true,
+    batchMaxCount: 1,
   })
 }
